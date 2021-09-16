3fdea80f61a756d3fd019e1cfba7e21ae338ab9a
Mark Melton <mark.melton@notlem.com>
2021-09-16 00:21:59 -0400

    WIP. SBS.

--- test/src/core/bit/test_sbs.cpp
+++ test/src/core/bit/test_sbs.cpp
@@ -4,6 +4,7 @@
 #include <gtest/gtest.h>
 #include "core/bit/sbs.h"
 #include "core/range/sample.h"
+#include "core/range/container.h"
 #include "core/mp/foreach.h"
 
 using namespace core;
@@ -24,9 +25,19 @@ void test_scalar() {
     }
 }
 
-template<class T>
+template<template<class...> class C, class T>
 void test_container() {
     auto g = cr::uniform<T>();
+    auto n = cr::uniform<size_t>(0, 33);
+    for (auto expected : cr::containerize<C<T>>(g, n) | v::take(NumberSamples)) {
+	std::stringstream ss;
+	to_sbs(ss, expected);
+
+	C<T> actual;
+	from_sbs(ss, actual);
+
+	EXPECT_EQ(actual, expected);
+    }
 }
 
 using BasicTypes = std::tuple<int,int64,uint,uint64,float,real>;
@@ -38,8 +49,26 @@ TEST(SBS, BasicTypes)
 	});
 }
 
+TEST(SBS, ContiguousContainers)
+{
+    core::mp::foreach<BasicTypes>([]<class T>() {
+	    test_container<std::vector,T>();
+	});
+}
+
 TEST(SBS, Containers)
 {
+    auto g = cr::uniform<int>();
+    auto n = cr::uniform<size_t>(0, 33);
+    for (auto expected : cr::containerize<list<int>>(g, n) | v::take(NumberSamples)) {
+	std::stringstream ss;
+	to_sbs(ss, expected);
+
+	list<int> actual;
+	from_sbs(ss, actual);
+
+	EXPECT_EQ(actual, expected);
+    }
 }
 
 int main(int argc, char *argv[])
