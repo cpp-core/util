5d3b57fb08ca781d06c64e8569f3f26bfbcff4d9
Mark Melton <mark.melton@notlem.com>
2021-09-17 14:41:25 -0400

    Added forward declaration in coro streams.

--- include/core/bit/sbs/string.h
+++ include/core/bit/sbs/string.h
@@ -5,12 +5,12 @@
 #include "core/bit/sbs/string_decl.h"
 #include "core/bit/sbs/pod_decl.h"
 
-void to_sbs(std::ostream& os, const string& str) {
+inline void to_sbs(std::ostream& os, const string& str) {
     to_sbs(os, str.size());
     to_sbs(os, &str[0], str.size());
 }
 
-void from_sbs(std::istream& is, string& str) {
+inline void from_sbs(std::istream& is, string& str) {
     size_t count;
     from_sbs(is, count);
     str.resize(count);
