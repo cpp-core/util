930039a3f896cf60a90e6029c07ff82ea46c98b3
Mark Melton <mark.melton@notlem.com>
2022-09-02 23:56:28 -0400

    Grand refactoring of file structure to rationalize organization.

--- include/core/bit/sbs/concept.h
+++ include/core/bit/sbs/concept.h
@@ -1,9 +1,9 @@
-// Copyright (C) 2021 by Mark Melton
+// Copyright (C) 2021, 2022 by Mark Melton
 //
 
 #pragma once
 #include <deque>
-#include "core/common.h"
+#include "core/utility/common.h"
 #include "core/mp/same.h"
 
 namespace core::sbs {
